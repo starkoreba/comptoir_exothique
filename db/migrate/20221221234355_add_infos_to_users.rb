fdac190088103aef4b53f3356de6f858c0ece1db
Maeva De Campos <kulwilcoox@gmail.com>
2022-12-22 00:51:54 +0100

    update devise user

---
+++ db/migrate/20221221234355_add_infos_to_users.rb
@@ -0,0 +1,10 @@
+class AddInfosToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :username, :string
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :birthdate, :date
+    add_column :users, :address, :string
+    add_column :users, :phone, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_21_221638) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_21_234355) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_21_221638) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
+    t.string "first_name"
+    t.string "last_name"
+    t.date "birthdate"
+    t.string "address"
+    t.string "phone"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
